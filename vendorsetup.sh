61480f6685d17421e6bb4b34404b758eaaf4c753
SALMANKHANPM <patanmohammadsalmankhan@gmail.com>
2022-01-16 04:41:35 +0530

    sdm660-common : clone tinyxml & airbnb-lottie

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,8 @@
 # Clone Atom-X Clang
 git clone --depth=1 https://gitlab.com/ElectroPerf/atom-x-clang.git  prebuilts/clang/host/linux-x86/clang-atom
+
+# Clone TinyXML
+git clone git@github.com:SALMANKHANPM/external_tinyxml.git external/tinyxml
+
+# Clone airbnb-lottie
+git clone git@github.com:SALMANKHANPM/android_external_airbnb-lottie.git -b 12 external/airbnb-lottie
