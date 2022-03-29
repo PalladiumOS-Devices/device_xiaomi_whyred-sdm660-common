14ebb963037cbc1386c8900847e306609630379c
SALMANKHANPM <patanmohammadsalmankhan@gmail.com>
2022-03-29 19:11:15 +0530

    whyred : clone protonclang , tinyxml , lottie

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,8 +1,8 @@
-# Clone Atom-X Clang
-git clone --depth=1 https://gitlab.com/ElectroPerf/atom-x-clang.git  prebuilts/clang/host/linux-x86/clang-atom
+# Clone Proton Clang
+git clone --depth=1 https://github.com/kdrag0n/proton-clang prebuilts/clang/host/linux-x86/clang-proton
 
 # Clone TinyXML
-git clone git@github.com:SALMANKHANPM/external_tinyxml.git external/tinyxml
+git clone https://github.com/SALMANKHANPM/external_tinyxml.git external/tinyxml
 
 # Clone airbnb-lottie
-git clone git@github.com:SALMANKHANPM/android_external_airbnb-lottie.git -b 12 external/airbnb-lottie
+git clone https://github.com/SALMANKHANPM/android_external_airbnb-lottie.git -b 12 external/airbnb-lottie
